d20e2c498f9f0a717ce2f4df64508abcd7af0f0c
bizbuddydev <bizbuddydev@gmail.com>
2024-11-30 13:21:09 -0800

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -11,6 +11,8 @@ from urllib.parse import quote
 st.set_page_config(page_title="BizBuddy", layout="wide", page_icon = "🤓")
 
 st.markdown("<h1 style='text-align: center;'>Welcome to BizBuddy: Let's Grow Your Digital Presence</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>Three Bears</h2>", unsafe_allow_html=True)
+
 
 def generate_seo_insights(search_data):
    # Prepare the search query list
